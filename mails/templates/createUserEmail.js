bc5589c696d013ba4ca6656ff3a105ae1c8a66dc
Sumesh9 <107548452+Sumesh8@users.noreply.github.com>
2024-07-22 01:34:43 +0530

    add email service

---
+++ mails/templates/createUserEmail.js
@@ -0,0 +1,84 @@
+/* eslint-disable camelcase */
+module.exports.createUser = (newUser) => {
+    const { name, new_password, subject } = newUser
+
+    const emailTemplate = `
+        <!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>${subject}</title>
+            <style>
+              /* Add your styles here */
+              body {
+                font-family: Arial, sans-serif;
+                background-color: #262833;
+                color: white;
+                margin: 0;
+                padding: 0;
+              }
+              p{
+                color : #FFFFFF
+              }
+              .container {
+                max-width: 600px;
+                margin: 0 auto;
+                padding: 20px;
+                background-color: #232324;
+              }
+              .header {
+                text-align: center;
+                background-color: #3a85a5;
+                border-radius: 7px;
+                color: #fff;
+                padding: 10px;
+              }
+              .content {
+                padding: 20px;
+              }
+              .footer {
+                text-align: center;
+                padding: 10px;
+                border-radius: 7px;
+                background-color: #3a85a5;
+                color: #fff;
+              }
+            </style>
+          </head>
+          <body>
+            <div class="container">
+              <img
+                src="cid:logo"
+                style="
+                  display: inline-block;
+                  max-width: 100%;
+                  height: auto;
+                  vertical-align: middle;
+                "
+              />
+              <div class="header">
+                <p style="font-size:20px; font-weight:bold;">${subject}</p>
+              </div>
+              <div class="content">
+                <p>Hello ${name},</p>
+                <p>
+                  Your account has been successfully created. Below is the password for
+                  your initial login:
+                </p>
+                <p><strong>${new_password}</strong></p>
+                <p>
+                  We recommend changing your password after the first login for security
+                  purposes.
+                </p>
+              </div>
+              <div class="footer">
+                <p>Thank you,<br />pick a book team</p>
+              </div>
+            </div>
+          </body>
+        </html>   
+    `
+
+    return emailTemplate
+}
--- mails/templates/mails.service.js
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-const sgMail = require("@sendgrid/mail");
-
-const { createUser } = require("./templates/create-user-email");
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
